f56fe5b674bafb2d6fe4ed0d4a9982f285e28f1d
Guillaume Marcel <marcelguillaume54@gmail.com>
2021-02-10 17:20:56 +0100

    add root

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -3,6 +3,7 @@ package ../views/View
 
 var db;
 
+
 func addUser(username string, password string)  {
 	user = User.create(username,password);
 	db.Create(user);
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+    //"fmt"
+    "net/http"
+    //"log"
+)
+
+
+func main() {
+    
+    http.HandleFunc("/api/login/", login)
+    http.HandleFunc("/api/users/", users)
+    http.HandleFunc("/api/comments/", comments)
+    http.HandleFunc("/api/articles/", articles)
+    
+
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+    
+    http.ListenAndServe(":80", nil)
+}
+
+
+
+
+func login(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    switch r.Method{
+        // Création d'un utilisateur
+        case "POST":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Success: User logged", id_user : "123"}`))
+        
+    }
+    
+    
+    
+}
+
+func users(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    switch r.Method{
+        // Création d'un utilisateur
+        case "POST":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Success: User created", id_user : "123"}`))
+        
+        case "GET":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "SHOW INFO", id_user : "123"}`))
+        
+        case "DELETE":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Sucess: Users deleted", id_user : "123"}`))
+        
+        case "PUT":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Sucess: Users updated", id_user : "123"}`))
+        
+    }
+
+    
+}
+
+func comments(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    switch r.Method{
+        // Création d'un utilisateur
+        case "POST":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Success: Add comment", id_user : "123"}`))
+        
+        case "GET":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Get comment", id_user : "123"}`))
+        
+        case "DELETE":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Sucess: Delete comment", id_user : "123"}`))
+        
+        case "PUT":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Sucess: Comment updated", id_user : "123"}`))
+        
+    }
+    
+    
+    
+    
+    
+}
+
+func articles(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+    switch r.Method{
+        // Création d'un utilisateur
+        case "POST":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Add article", id_user : "123"}`))
+        
+        case "GET":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "List of articles", id_user : "123"}`))
+        
+        case "DELETE":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Sucess: Delete article", id_user : "123"}`))
+        
+        case "PUT":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "Sucess: Article updated", id_user : "123"}`))
+        
+    }
+    
+    
+} 
