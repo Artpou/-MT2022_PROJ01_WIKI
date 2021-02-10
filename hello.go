ca3b1092508dda1e0810041985ff19b1a3aeb63a
Poullin <a.poullin@reussir.fr>
2021-02-10 15:03:27 +0100

    Feat add model

--- hello.go
+++ hello.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
 }
