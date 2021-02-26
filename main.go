669a92bc953003ac4c779b98e442b2e0abcd5f65
Poullin <a.poullin@reussir.fr>
2021-02-26 20:28:51 +0100

    Feat update router

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 
+	"github.com/Artpou/wiki_golang/controllers"
 	"github.com/gorilla/mux"
 )
 
@@ -13,19 +14,34 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/api/login/", login)
-	router.HandleFunc("/api/users/", users)
 	router.HandleFunc("/api/comments/", comments)
 	router.HandleFunc("/api/articles/", articles)
+
+	/* User methods */
+	router.HandleFunc("/api/users", ShowInfo).Methods("GET")
+	router.HandleFunc("/api/users", UpdateInfo).Methods("POST")
+	router.HandleFunc("/api/users", DeleteSelf).Methods("DELETE")
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-
 }
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome")
+}
+
+func ShowInfo(w http.ResponseWriter, r *http.Request) {
+	controllers.GetSelf(w, r)
+}
+
+func UpdateInfo(w http.ResponseWriter, r *http.Request) {
+	controllers.UpdateSelf(w, r)
+}
+
+func DeleteSelf(w http.ResponseWriter, r *http.Request) {
+	controllers.DeleteSelf(w, r)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -35,33 +51,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte(`{"message": "Success: User logged", id_user : "123"}`))
-
 	}
-
-}
-
-func users(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	switch r.Method {
-	// Création d'un utilisateur
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "Success: User created", id_user : "123"}`))
-
-	case "GET":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "SHOW INFO", id_user : "123"}`))
-
-	case "DELETE":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "Sucess: Users deleted", id_user : "123"}`))
-
-	case "PUT":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message": "Sucess: Users updated", id_user : "123"}`))
-
-	}
-
 }
 
 func comments(w http.ResponseWriter, r *http.Request) {
