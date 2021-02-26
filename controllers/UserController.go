97aac9473d47658e5925fb5a0237f78f87581340
Poullin <a.poullin@reussir.fr>
2021-02-26 20:27:17 +0100

    Feat add all Controller

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,11 +1,50 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/Artpou/wiki_golang/models"
 	"github.com/Artpou/wiki_golang/views"
 )
 
-func addUser(username string, password string) {
-	user = User.create(username, password)
-	db.Create(user)
-	views.Test()
+func AddUser(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	user := models.NewUser("test", "1234")
+	w.Write([]byte(views.AddUser(*user)))
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	user := models.NewUser("test", "1234")
+	w.Write([]byte(views.ShowUser(*user)))
+}
+
+func GetSelf(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	user := models.NewUser("test", "1234")
+	w.Write([]byte(views.ShowUser(*user)))
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	user := models.NewUser("test", "1234")
+	w.Write([]byte(views.DeleteUser(*user)))
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	user := models.NewUser("test", "1234")
+	w.Write([]byte(views.DeleteUser(*user)))
+}
+
+func UpdateSelf(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	user := models.NewUser("test", "1234")
+	w.Write([]byte(views.UpdateUser(*user)))
+}
+
+func DeleteSelf(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	user := models.NewUser("test", "1234")
+	w.Write([]byte(views.DeleteUser(*user)))
 }
