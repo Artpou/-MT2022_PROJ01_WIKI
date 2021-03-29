ab63376de6d08550250c6ee84772d50337600fb3
Arthur PC2 <arthur.poullin99@gmail.com>
2021-03-29 16:24:41 +0200

    Feat update Role

--- models/User.go
+++ models/User.go
@@ -2,22 +2,31 @@ package models
 
 import (
 	"time"
+
 	"github.com/Artpou/wiki_golang/handler"
 	_ "github.com/jinzhu/gorm"
 )
 
+type Role int
+
+const (
+	UserRole  = iota
+	AdminRole = iota
+)
+
 type User struct {
-	ID           uint `gorm:"primaryKey"`
+	ID           uint   `gorm:"primaryKey"`
 	Username     string `gorm:"unique;not null;size:255"`
 	Password     string `gorm:"not null;size:255"`
+	Role         Role
 	CreationDate time.Time
 	LatestUpdate time.Time
 }
 
-
 func NewUser(username string, password string) *User {
 	hash, _ := handler.HashPassword(password)
 	user := User{Username: username, Password: hash}
+	user.Role = UserRole
 	user.CreationDate = time.Now()
 	user.LatestUpdate = time.Now()
 	return &user
